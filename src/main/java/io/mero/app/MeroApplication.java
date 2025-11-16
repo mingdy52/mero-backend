e96cdbb5f40659e2375f7f7c160bae7492a2129f
mingdy <mincount552@gmail.com>
2025-11-16 22:50:33 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/io/mero/app/MeroApplication.java
@@ -0,0 +1,13 @@
+package io.mero.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mero/app/MeroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mero.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
