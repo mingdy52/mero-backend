56e70a966d6a7b66a9df8f530d1389601aa7b7ac
mingdy <mincount552@gmail.com>
2025-11-21 19:29:49 +0900

    test: AuthControllerTest에 Security 필터 비활성화

--- src/test/java/io/mero/app/domain/user/controller/AuthControllerTest.java
+++ src/test/java/io/mero/app/domain/user/controller/AuthControllerTest.java
@@ -9,6 +9,7 @@ import io.mero.app.global.enums.Timezone;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest {
 
     @Autowired
